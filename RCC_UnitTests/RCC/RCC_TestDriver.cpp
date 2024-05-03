b5861f0080776d70022a8f49687e29724127c8bd
Eduardo Heredia Gonzalez <EduardoHerediaGonzalez@gmail.com>
2024-05-03 12:22:42 -0600

    Initial project version.

---
+++ RCC_UnitTests/RCC/RCC_TestDriver.cpp
@@ -0,0 +1,77 @@
+/**
+ * @file    COMPNAME_TestDriver.cpp
+ *
+ * @brief   Código de ensayos para el componente COMPNAME.
+ *
+ * @note
+ *        ______________
+ *        TBD_E
+ *        Encargado de
+ *	      ensayos
+ *
+ * @version
+ * - TBD_E -- DD/MM/YYYY -- Actual
+ * 	- Versión inicial.
+ *
+ */
+
+
+/* system headers */
+#include "CppUTest/TestHarness.h"
+extern "C"
+{
+/* System headers written in C */
+
+}
+
+/* used headers */
+extern "C"
+{
+/* Used headers written in C */
+}
+
+/******************** Start of Code Under Test ********************/
+extern "C"
+{
+
+}
+/******************** End of Code Under Test ********************/
+
+
+/*[EXPORTED]****************************************************************************/
+
+/* public_variables */
+
+/*[INTERNAL]****************************************************************************/
+
+/* internal__pre-processor macros */
+
+
+/* internal__types */
+
+
+/* private__variables */
+
+
+/* private__functions */
+
+
+/* public_functions */
+TEST_GROUP(TESTING_TEMPLATE)
+{
+    void setup()
+    {
+    	/* Code intended for Test Group initializing */
+    }
+    void teardown()
+    {
+    	/* Code intended for Test Group destruction */
+    }
+};
+
+TEST(TESTING_TEMPLATE, TestId)
+{
+	FAIL("Write tests");
+}
+
+/* EOF */
---
+++ src/main.c
@@ -0,0 +1,44 @@
+/**
+ * Copyright © 2023 
+ *
+ * @file    main.c
+ * @note
+ *        ______________      
+ *        Eduardo
+ *        Heredia Gonzalez
+ *        Diseñador           
+ *
+ * @brief   main 
+ * @version No oficial
+ *     
+ */
+/* system headers */
+#include "stdio.h"
+
+/* used headers */
+
+/* own headers */
+
+/*[EXPORTED]****************************************************************************/
+
+/* public_variables */
+
+/*[INTERNAL]****************************************************************************/
+
+/* internal__pre-processor macros */
+
+/* internal__types */
+
+/* private__variables */
+
+/* private__functions */
+
+/* public_functions */
+int main(void)
+{
+	printf("Hello world!!!");
+
+	return 0;
+}
+
+/* EOF */
